b2e9687e4bb75c19ace9c883100607ec1a602d42
Zhan.Shi <g.shizhan.g@gmail.com>
2018-03-14 09:41:40 +0800

    update script

--- run.cmd
+++ run.cmd
@@ -2,4 +2,4 @@
 @rem set MINIO_ACCESS_KEY=hust& set MINIO_SECRET_KEY=hust2018& minio.exe -C ./ server c:/minio/root
 set MINIO_ACCESS_KEY=hust
 set MINIO_SECRET_KEY=hust2018
-minio.exe -C ./ server c:/minio/root
+minio.exe -C ./ server ./root
