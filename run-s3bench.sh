8404090686a33f5566edd00f79f3384b6484e7ce
Shi.Zhan <g.shizhan.g@gmail.com>
2019-09-18 12:38:30 +0800

    simplify benchmark script

--- run-s3bench.sh
+++ run-s3bench.sh
@@ -4,8 +4,11 @@
 # 1. use loop structure to generate test batch (E.g.: to re-evaluate s3 server under the same configuration, or to gather data from a range of parameters)
 # 2. use redirection for storing program output to text files
 
+s3bench=~/go/bin/s3bench
+
 if [ -n "$GOPATH" ]; then
-    $GOPATH/bin/s3bench -accessKey=hust -accessSecret=hust2019 -bucket=loadgen -endpoint=http://127.0.0.1:9000 -numClients=10 -numSamples=100 -objectNamePrefix=loadgen -objectSize=1024
-else
-    ~/go/bin/s3bench -accessKey=hust -accessSecret=hust2019 -bucket=loadgen -endpoint=http://127.0.0.1:9000 -numClients=10 -numSamples=100 -objectNamePrefix=loadgen -objectSize=1024
+    s3bench=$GOPATH/bin/s3bench
 fi
+
+$s3bench -accessKey=hust -accessSecret=hust2019 -bucket=loadgen -endpoint=http://127.0.0.1:9000 -numClients=10 -numSamples=100 -objectNamePrefix=loadgen -objectSize=1024
+
