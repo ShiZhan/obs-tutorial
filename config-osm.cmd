bd2b7200400c292361089259dac4e6ef9deb13bb
Zhan.Shi <g.shizhan.g@gmail.com>
2020-04-25 14:07:29 +0800

    all scripts ready

--- config-osm.cmd
+++ config-osm.cmd
@@ -1,2 +1,3 @@
 osm config set-context osm-minio --provider=s3 --s3.access_key_id=hust --s3.secret_key=hust_obs --s3.endpoint=http://127.0.0.1:9000
 osm config view
+pause
--- run-mock-s3.cmd
+++ run-mock-s3.cmd
@@ -1,2 +1,1 @@
-#!/bin/sh
 python mock_s3/main.py --port 9000 --root .root
