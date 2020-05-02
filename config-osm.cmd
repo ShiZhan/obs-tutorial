1b07305b58ddfe03a2564e252b8a409049012042
Shi.Zhan <g.shizhan.g@gmail.com>
2020-05-03 00:02:14 +0800

    Script comments.

--- config-osm.cmd
+++ config-osm.cmd
@@ -3,6 +3,6 @@ rem Setup alias for using local configuration
 set SCRIPT_DIR=%~dp0
 @doskey osm=osm --osmconfig %SCRIPT_DIR%osm.conf $*
 
+rem Create OSM configuration and save to 'osm.conf'
 osm --osmconfig %SCRIPT_DIR%osm.conf config set-context osm-minio --provider=s3 --s3.access_key_id=hust --s3.secret_key=hust_obs --s3.endpoint=http://127.0.0.1:9000
 osm --osmconfig %SCRIPT_DIR%osm.conf config view
-pause
--- config-osm.sh
+++ config-osm.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
-# OSM default location
-osm=~/go/bin/osm
+# Get script location
+called=$_
+if [[ $called == $0 ]]; then
+    SCRIPT_PATH="$0"
+else
+    SCRIPT_PATH="${BASH_SOURCE[0]}"
+fi
+SCRIPT_DIR=$( cd "$(dirname $SCRIPT_PATH )" && pwd )
 
-# Update OSM executable path based on GOPATH
+# Get OSM location (GOPATH default to ~/go)
+osm=~/go/bin/osm
 if [ -n "$GOPATH" ]; then
     osm=$GOPATH/bin/osm
 fi
 
 # Setting up OSM with local configuration file "osm.conf"
-$osm --osmconfig osm.conf config set-context osm-minio --provider=s3 --s3.access_key_id=hust --s3.secret_key=hust_obs --s3.endpoint=http://127.0.0.1:9000
-$osm --osmconfig osm.conf config view
+$osm --osmconfig $SCRIPT_DIR/osm.conf \
+    config set-context osm-minio \
+        --provider=s3 \
+        --s3.access_key_id=hust \
+        --s3.secret_key=hust_obs \
+        --s3.endpoint=http://127.0.0.1:9000
+
+# Check this configuration
+$osm --osmconfig $SCRIPT_DIR/osm.conf \
+    config view
 
 # Prepare alias for simplified command line
-called=$_
-if [[ $called == $0 ]]; then
-  SCRIPT_PATH="$0"
-else
-  SCRIPT_PATH="${BASH_SOURCE[0]}"
-fi
-SCRIPT_DIR=$( cd "$(dirname $SCRIPT_PATH )" && pwd )
 alias osm='osm --osmconfig $SCRIPT_DIR/osm.conf'
