083b5e99effd5d77b1d5f5d23561c386f8085f82
Zhan.Shi <g.shizhan.g@gmail.com>
2020-09-23 23:39:24 +0800

    build-in metrics

--- run-minio.cmd
+++ run-minio.cmd
@@ -4,6 +4,9 @@
 set MINIO_ACCESS_KEY=hust
 set MINIO_SECRET_KEY=hust_obs
 
+@rem Export metrics
+set MINIO_PROMETHEUS_AUTH_TYPE=public
+
 @rem Use "-C" flag to store configuration file in local directory "./".
 @rem Use server command to start object storage server with "./root" as root directory, in which holds all buckets and objects.
 minio.exe -C ./ server ./root
--- run-minio.sh
+++ run-minio.sh
@@ -4,6 +4,9 @@
 export MINIO_ACCESS_KEY=hust
 export MINIO_SECRET_KEY=hust_obs
 
+# Export metrics
+export MINIO_PROMETHEUS_AUTH_TYPE="public"
+
 # Use "-C" flag to store configuration file in local directory "./".
 # Use server command to start object storage server with "./root" as root directory, in which holds all buckets and objects.
 ./minio -C ./ server ./root
