9062ac952035d9177d2e4ec7b4470b27a96038b6
Shi.Zhan <g.shizhan.g@gmail.com>
2021-06-07 21:26:33 +0800

    change bc to python for portability

--- test-put-100.sh
+++ test-put-100.sh
@@ -3,6 +3,6 @@ for obj in {0..99}; do
     t0=`date +%s.%N`
     curl -s -X PUT -d "..." http://127.0.0.1:9000/hello/$obj.bin
     t1=`date +%s.%N`
-    printf "%d, %s\n" $obj `bc <<< "$t1 - $t0"`
+    printf "%d, %s\n" $obj `python -c "print(${t1} - ${t0})"`
 done
 printf "\n"
