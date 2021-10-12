26fbbc87aaf5efbc6481fd2f0badfe1d1c342086
Zhan.Shi <g.shizhan.g@gmail.com>
2021-10-12 16:26:59 +0800

    bump to new version of minio, adjust scripts.

--- run-minio.cmd
+++ run-minio.cmd
@@ -9,7 +9,7 @@ set MINIO_PROMETHEUS_AUTH_TYPE=public
 
 @rem Use "-C" flag to store configuration file in local directory "./".
 @rem Use server command to start object storage server with "./root" as root directory, in which holds all buckets and objects.
-minio.exe -C ./ server ./root
+minio.exe -C ./ server ./root --console-address ":9090"
 
 @rem Run above task in one command line.
 @rem set MINIO_ROOT_USER=hust& set MINIO_ROOT_PASSWORD=hust_obs& minio.exe -S ./certs server ./root
--- run-minio.sh
+++ run-minio.sh
@@ -9,7 +9,7 @@ export MINIO_PROMETHEUS_AUTH_TYPE="public"
 
 # Use "-C" flag to store configuration file in local directory "./".
 # Use server command to start object storage server with "./root" as root directory, in which holds all buckets and objects.
-./minio -C ./ server ./root
+./minio -C ./ server ./root --console-address ":9090"
 
 # Run above task in one command line.
 # export MINIO_ROOT_USER=hust && export MINIO_ROOT_PASSWORD=hust_obs && ./minio -C ./ server ./root
