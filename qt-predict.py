68bdc690aec35e7c768a021a17e89f022658277b
Zhan.Shi <g.shizhan.g@gmail.com>
2022-11-15 17:02:40 +0800

    queueing theory analysis

--- qt-predict.py
+++ qt-predict.py
@@ -19,21 +19,65 @@ def arrival(x, λ):
 x1 = np.linspace(0, 99, 100)
 
 # 平均到达率、服务率初始值
-init_λ = 10
-init_μ = 15
+init_arrival_rate = 10
 
 # 绘图
 fig1, ax1 = plt.subplots()
-line1, = ax1.plot(x1, arrival(x1, init_λ), lw=2)
+line1, = ax1.plot(x1, arrival(x1, init_arrival_rate), lw=2)
 ax1.set_xlabel('arrival speed')
 
 # 绘图区域留空给滑动条
 fig1.subplots_adjust(bottom=0.25)
 
-# λ设置滑动条
-ax_λ = fig1.add_axes([0.1, 0.1, 0.5, 0.03])
+# 设置请求到达率滑动条
+ax_arrival_rate = fig1.add_axes([0.1, 0.1, 0.5, 0.03])
+slider_arrival_rate = Slider(
+    ax=ax_arrival_rate,
+    label='arrival rate',
+    valmin=0,
+    valmax=50,
+    valstep=1,
+    valinit=init_arrival_rate,
+)
+
+# 刷新绘图
+def update1(val):
+    line1.set_ydata(arrival(x1, slider_arrival_rate.val))
+    fig1.canvas.draw_idle()
+    line2.set_ydata(np.exp(-x2))
+    fig2.canvas.draw_idle()
+
+# 注册参数刷新函数
+slider_arrival_rate.on_changed(update1)
+
+# λ重置按钮
+resetax1 = fig1.add_axes([0.8, 0.025, 0.1, 0.04])
+button1 = Button(resetax1, 'Reset', hovercolor='0.975')
+
+def reset1(event):
+    slider_arrival_rate.reset()
+
+button1.on_clicked(reset1)
+
+# *排队论预测延迟*
+
+def queueing_model(x, λ, μ):
+    return 1 - np.exp(-1*(μ-λ)*x)
+
+x2 = np.linspace(0, 10, 100)
+
+init_λ = 10
+init_μ = 15
+
+fig2, ax2 = plt.subplots()
+line2, = ax2.plot(x2, queueing_model(x2, init_λ, init_μ), lw=2)
+ax2.set_xlabel('latency')
+
+fig2.subplots_adjust(bottom=0.25)
+
+λ_ax = fig2.add_axes([0.1, 0.1, 0.3, 0.03])
 λ_slider = Slider(
-    ax=ax_λ,
+    ax=λ_ax,
     label='λ',
     valmin=0,
     valmax=50,
@@ -41,41 +85,9 @@ ax_λ = fig1.add_axes([0.1, 0.1, 0.5, 0.03])
     valinit=init_λ,
 )
 
-# 刷新绘图
-def update1(val):
-    line1.set_ydata(arrival(x1, λ_slider.val))
-    fig1.canvas.draw_idle()
-    line2.set_ydata(np.exp(-x2))
-    fig2.canvas.draw_idle()
-
-# 注册参数刷新函数
-λ_slider.on_changed(update1)
-
-# λ重置按钮
-resetax1 = fig1.add_axes([0.8, 0.025, 0.1, 0.04])
-button1 = Button(resetax1, 'Reset', hovercolor='0.975')
-
-def reset1(event):
-    λ_slider.reset()
-
-button1.on_clicked(reset1)
-
-# *排队论预测延迟*
-
-def queueing_model(x):
-    return 1 - np.exp(-x)
-
-x2 = np.linspace(0, 10, 100)
-
-fig2, ax2 = plt.subplots()
-line2, = ax2.plot(x2, queueing_model(x2), lw=2)
-ax2.set_xlabel('latency')
-
-fig2.subplots_adjust(bottom=0.25)
-
-ax_μ = fig2.add_axes([0.6, 0.1, 1.0, 0.03])
+μ_ax = fig2.add_axes([0.5, 0.1, 0.3, 0.03])
 μ_slider = Slider(
-    ax=ax_μ,
+    ax=μ_ax,
     label='μ',
     valmin=0,
     valmax=50,
@@ -84,18 +96,20 @@ ax_μ = fig2.add_axes([0.6, 0.1, 1.0, 0.03])
 )
 
 def update2(val):
-    line2.set_ydata(queueing_model(x2))
+    line2.set_ydata(queueing_model(x2, λ_slider.val, μ_slider.val))
     fig2.canvas.draw_idle()
 
+λ_slider.on_changed(update2)
 μ_slider.on_changed(update2)
 
 # λ重置按钮
 resetax2 = fig2.add_axes([0.8, 0.025, 0.1, 0.04])
 button2 = Button(resetax2, 'Reset', hovercolor='0.975')
 
-def reset(event):
+def reset2(event):
+    λ_slider.reset()
     μ_slider.reset()
 
-button2.on_clicked(reset)
+button2.on_clicked(reset2)
 
 plt.show()
